feb63c6a17986c0ee791c3996b6215ecb88c171d
ChrisPytel <chris_pytel@hotmail.com>
2024-03-23 20:14:18 -0400

    implement JS to update html counter to tweet character length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,23 +3,32 @@
 $(document).ready(function() {  
   console.log("Hey is the tweet-counter script running?\nYa better go catch it :)");
 
+  $("#tweet-text").on('input', function(event){
+  // console.log(`input for event is:`, event);
 
-  const inputField = document.querySelector("#tweet-text"); 
- 
-  inputField.addEventListener('input', function(event){
-  // console.log(`input for event is:`, event.data);
-  // console.log(`this is:`, this);
-
-  const tweetMessage = event.target.value; //stores the current content of the textarea as a variable
+  //stores the current content of the textarea as a variable
+  //updates everytime anything new is added
+  const tweetMessage = event.target.value; 
   console.log(`Content: `, tweetMessage);
-  console.log(`Counter is at: `, tweetMessage.length);
+  // console.log(`Counter is at: `, tweetMessage.length);
+
+  const tweetLengthCap = 140;
+  const counterUpdate = tweetLengthCap - tweetMessage.length;  
+  console.log(counterUpdate);
+  
+  const counterElement = document.querySelector('.counter');
+  console.log(`counterElementis `, counterElement.value);
+
+  counterElement.value = counterUpdate; //appends the counter result to the html element's value
+
+
+  if (counterUpdate < 0) { // when character count is exceeded
+    counterElement.style.color = '#ff0000';
+  } else { // when below the max length
+    counterElement.style.color = `#4056A1`;
+    console.log(`underlimit`);
+  }
 
   });
-  
-  //Jquery experiment
 
-  // $("#tweet-text").on('input', function(event){
-  // console.log(`input for event is:`, event);
-  // // console.log(`this is:`, this);
-  // });
 });
