00d4bd00b4be96ae6df943499ff29e34ab9ab0e7
ChrisPytel <chris_pytel@hotmail.com>
2024-03-31 00:18:24 -0400

    update fetching tweetlibrary to use $.ajax GET instead of temporary data, add timeago library to index.html head

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,38 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {  
+$(document).ready(function() {
 
-// OOPS got ahead of myself - not using file system stuff just yet
+  /* Temporary Datastructure before AJAX Server fetching
 
-// const fs = require('fs');
-// const tweetLibrary = require('../../server/data-files/initial-tweets.json');
-
-
-
-const tempTweetData = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-},
-{
-  "user": {
-    "name": "Queen Victoria",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@BritanniaRules"
+  const tempTweetData = [{
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "We are not interested in the possibilities of defeat. They do not exist."
-  },
-  "created_at": 1711379442671
-}
-];
+  {
+    "user": {
+      "name": "Queen Victoria",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@BritanniaRules"
+    },
+    "content": {
+      "text": "We are not interested in the possibilities of defeat. They do not exist."
+    },
+    "created_at": 1711379442671
+  }
+  ]; */
 
-//------------------  Section for Functions ------------------
+  //------------------  Section for Functions ------------------
 
-const createTweetElement = function(tweetObj) {  
-  console.log(`Running createTweetElement`);
-
-  const markupStructure = $(`
+  const createTweetElement = function(tweetObj) {
+    const markupStructure = $(`
   <article class="tweet">
     <header>
       <img src= ${tweetObj.user.avatars}>   
@@ -55,48 +48,60 @@ const createTweetElement = function(tweetObj) {
     <div class="divider">
     </div>
     <footer>
-      <h6>${tweetObj.created_at}</h6>
+      <h6>${timeago.format(tweetObj.created_at)}</h6>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
       <i class="fa-solid fa-heart"></i>
     </footer> 
   </article>
   `);
-  console.log(`Our markupStructure is: `, markupStructure);
-  return markupStructure;
-};
+    // console.log(`Our markupStructure is: `, markupStructure);
+    return markupStructure;
+  };
 
 
-const renderTweets = function(allTweets) {
+  const renderTweets = function(allTweets) {
   // console.log(`tweetLibrary is: `, allTweets);
-  allTweets.forEach((tweet,index) => {
-    console.log(`Tweet #${index+1} is:`, tweet);
-    // calls createTweetElement for each tweet
-    const element = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('.tweet-container').append(element);        
-  }); 
-};
+    allTweets.forEach((tweet,index) => {
+      // console.log(`Tweet #${index + 1} is:`, tweet);
+      const element = createTweetElement(tweet); // calls createTweetElement for each tweet
+      $('.tweet-container').append(element); // takes return value and appends it to the tweets container
+    });
+  };
 
 
+  const loadTweets = function() {
+    const route = `/tweets`;
+    $.ajax({
+      url: route,
+      method: 'GET',
+      success: (tweetLibraryData) => {
+        console.log(`tweetLibraryData is: `, tweetLibraryData);
+        renderTweets(tweetLibraryData);   // passes in library data to render instead of temporary code
+      // renderTweets(tempTweetData);     // begins process of loading all tweets (BEFORE AJAX)
+      },
+      error: (error) => {
+        console.log(`$.ajax GET request error when getting info from ${route}`, error);
+      }
+    });
+  };
+
+  loadTweets(); // begins process of loading all tweets using AJAX logic
 
 
+  //------------------  Section for Listeners ------------------
 
-renderTweets(tempTweetData); // begins process of loading all tweets
+  //listens to the form for when submit called on the button corresponding to form
+  $('#tweet-form').on('submit', function(event) {
+    console.log("Handler for `submit` called.");
+    event.preventDefault();
 
-//------------------  Section for Listeners ------------------
+    $.ajax('/tweets', {method: 'POST'});
+    console.log(`$(this) is: `, $(this));
+    const $tweetInput = $(this).serialize();
+    console.log(`$data is: `, $tweetInput);
 
-//listens to the form for when submit called on the button corresponding to form
-$('#tweet-form').on('submit', function(event){
-  console.log( "Handler for `submit` called." );
-  event.preventDefault();
-
-  $.ajax('/tweets', {method: 'POST'});
-  console.log(`$(this) is: `, $(this));
-  const $tweetInput = $(this).serialize();  
-  console.log(`$data is: `, $tweetInput);
-
-  $.post('/tweets', $tweetInput, console.log('sent to /tweets'));
-});
+    $.post('/tweets', $tweetInput, console.log('sent to /tweets'));
+  });
 
 }); // --------- end of $(document).ready ---------
